fa28f96ff01f5b2ba1b633572860437bbdafa416
Ivan Quintana <ivanquisco@gmail.com>
2021-11-29 15:23:24 -0600

    Repository created

---
+++ SpotifySpike/SpotifySpikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifySpikeApp.swift
+//  SpotifySpike
+//
+//  Created by Ivan Quintana on 29/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SpotifySpikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
