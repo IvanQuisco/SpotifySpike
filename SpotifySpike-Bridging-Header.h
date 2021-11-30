ffacc9fa7e5fbdbe1d0a614d4490a5428f761573
Ivan Quintana <ivanquisco@gmail.com>
2021-11-29 18:10:30 -0600

    Project configuration to support spotify sdk

---
+++ SpotifySpike-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SpotifySpike-Bridging-Header.h
+//  SpotifySpike
+//
+//  Created by Ivan Quintana on 29/11/21.
+//
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#ifndef SpotifySpike_Bridging_Header_h
+#define SpotifySpike_Bridging_Header_h
+
+
+#endif /* SpotifySpike_Bridging_Header_h */
